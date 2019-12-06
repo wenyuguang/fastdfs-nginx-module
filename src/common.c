44bb52bfd65c7948e8ce3504a3013b2facd40681
wenyuguang <wenyuguang@zchtwork.com>
2019-12-06 11:37:42 +0800

    代码调整

--- src/common.c
+++ src/common.c
@@ -1,12 +1,3 @@
-/**
-* Copyright (C) 2008 Happy Fish / YuQing
-*
-* FastDFS may be copied only under the terms of the GNU General
-* Public License V3, which may be found in the FastDFS source kit.
-* Please visit the FastDFS Home Page http://www.csource.org/ for more detail.
-**/
-
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
